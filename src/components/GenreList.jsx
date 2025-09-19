e56b24a86bbaf7cfd4126babe61ff3c4c2a9bf0b
frontend-ashwamedhgroup <frontend@ashwamedhgroup.com>
2025-09-19 16:32:55 +0530

    Cleanup the genres

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,5 +1,12 @@
 import useGenres from "../hooks/useGenres";
-import { HStack, Button, Image, List, ListItem } from "@chakra-ui/react";
+import {
+  HStack,
+  Button,
+  Image,
+  List,
+  ListItem,
+  Heading,
+} from "@chakra-ui/react";
 import getCroppedUrl from "../services/image-url";
 import GenreSkeleton from "./GenreSkeleton";
 
@@ -8,29 +15,36 @@ const GenreList = ({ selectedGenre, onSelectGenre }) => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17];
 
   return (
-    <List>
-      {isLoading &&
-        skeletons.map((skeleton) => <GenreSkeleton key={skeleton} />)}
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY={"5px"}>
-          <HStack>
-            <Image
-              boxSize="40px"
-              borderRadius={8}
-              src={getCroppedUrl(genre.image_background)}
-            />
-            <Button
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-              onClick={() => onSelectGenre(genre)}
-              variant="link"
-              fontSize="lg"
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {isLoading &&
+          skeletons.map((skeleton) => <GenreSkeleton key={skeleton} />)}
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY={"5px"}>
+            <HStack>
+              <Image
+                boxSize="40px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedUrl(genre.image_background)}
+              />
+              <Button
+                whiteSpace="normal"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                variant="link"
+                fontSize="lg"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
