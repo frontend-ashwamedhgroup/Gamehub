987f157862bb3689db3b9a7ec56dc60f0a72f05b
frontend-ashwamedhgroup <frontend@ashwamedhgroup.com>
2025-09-20 12:23:03 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/gaming.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const NavBar = ({ onSearch }) => {
   return (
     <HStack padding={"10px"}>
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} borderRadius={10} boxSize="60px" />
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
